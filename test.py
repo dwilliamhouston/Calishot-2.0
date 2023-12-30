7ca0ae16a72d6ef55eaa1ef2e001aeecfb3d9155
dwilliamhouston <david@dwilliamshome.com>
2023-12-29 21:26:01 -0600

    Default to run all functions.

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ run_index_to_json = True
 ####################################################
 # Call search_by_country Function for each Country #
 ####################################################
-run_search_by_country = False
+#run_search_by_country = False
 if run_search_by_country:
     print ("Running search_by_country...")
     calibre_by_country('AU')
@@ -36,7 +36,7 @@ if run_search_by_country:
 ##############################################
 # Call book_search Function for each Country #
 ##############################################
-run_book_search = False
+#run_book_search = False
 if run_book_search:
     print ("Running run_book_search...")
     book_search('AU')
@@ -51,7 +51,7 @@ if run_book_search:
 ####################################
 # Call check_calibre_list Function #
 ####################################
-run_check_calibre_list = False
+#run_check_calibre_list = False
 if run_check_calibre_list:
     print ("Running run_check_calibre_list...")
     check_calibre_list()
@@ -59,7 +59,7 @@ if run_check_calibre_list:
 ##################################
 # Call output_online_db Function #
 ##################################
-run_output_online_db = False
+#run_output_online_db = False
 if run_output_online_db:
     print ("Running output_online_db...")
     output_online_db()
@@ -67,7 +67,7 @@ if run_output_online_db:
 ##################################
 # Call index_site_list Function #
 ##################################
-run_index_site_list = False
+#run_index_site_list = False
 if run_index_site_list:
     print ("Running index_site_list...")
     index_site_list('online.txt')
@@ -75,7 +75,7 @@ if run_index_site_list:
 #####################################
 # Call index_site_list_seq Function #
 #####################################
-run_index_site_list_seq = False
+#run_index_site_list_seq = False
 if run_index_site_list_seq:
     print ("Running index_site_list_seq...")
     index_site_list_seq('online.txt')
@@ -83,7 +83,7 @@ if run_index_site_list_seq:
 #############################
 # Call build_index Function #
 #############################
-run_build_index = False
+#run_build_index = False
 if run_build_index:
     print ("Running run_index_site_list...")
     build_index()
@@ -91,7 +91,7 @@ if run_build_index:
 ###########################
 # Call get_stats Function #
 ###########################
-run_get_stats = False
+#run_get_stats = False
 if run_get_stats:
     print ("Running get_stats...")
     get_stats()
