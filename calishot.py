c34ce4847d24d18d57a53f3f87bbcbcd45134ce2
dwilliamhouston <david@dwilliamshome.com>
2025-06-16 10:00:22 -0500

    Added all ISO country codes.
    Signed-off-by: dwilliamhouston <david@dwilliamshome.com>

--- calishot.py
+++ calishot.py
@@ -11,7 +11,6 @@ logging.basicConfig(filename='shodantest.log', encoding='utf-8', level=logging.E
 #####################
 # Flags for testing #
 #####################
-run_search_by_country_test = False
 run_search_by_country = True
 run_book_search = True
 run_check_calibre_list = True
@@ -25,144 +24,77 @@ run_index_to_json = True
 ####################################################
 # Call search_by_country Function for each Country #
 ####################################################
-if run_search_by_country_test:
-    print ("Running search_by_country...")
-    logging.info("Running search_by_country...")
-    calibre_by_country('US')  
-
 if run_search_by_country:
-    print ("Running search_by_country...")
+    print("Running search_by_country...")
     logging.info("Running search_by_country...")
-    calibre_by_country('US')    
-    calibre_by_country('CN')
-    calibre_by_country('DE')
-    calibre_by_country('GB')
-    calibre_by_country('SG')
-    calibre_by_country('JP')
-    calibre_by_country('ID')
-    calibre_by_country('CA')
-    calibre_by_country('HK')
-    calibre_by_country('IT')
-    calibre_by_country('HK')
-    calibre_by_country('AE')
-    calibre_by_country('ES')
-    calibre_by_country('IT')
-    calibre_by_country('KR')
-    calibre_by_country('OM')
-    calibre_by_country('FR')
-    calibre_by_country('NL')
-    calibre_by_country('TH')
-    calibre_by_country('PH')
-    calibre_by_country('MY')
-    calibre_by_country('AU')
-    calibre_by_country('IQ')
-    calibre_by_country('SA')
-    calibre_by_country('PK')
-    calibre_by_country('NP')
-    calibre_by_country('ZA')
-    calibre_by_country('IN')
-    calibre_by_country('RU')
-    calibre_by_country('NZ')
-    calibre_by_country('QA')
-    calibre_by_country('AR')
-    calibre_by_country('MX')
-    calibre_by_country('TW')
-    calibre_by_country('CH')
-    calibre_by_country('CL')
-    calibre_by_country('HU')
-    calibre_by_country('KE')
-    calibre_by_country('SE')
-    calibre_by_country('BR')
-    calibre_by_country('VN')
-    calibre_by_country('BE')
-    calibre_by_country('IE')
-    calibre_by_country('NO')
-    calibre_by_country('PE')
-    calibre_by_country('AT')
-    calibre_by_country('BA')
-    calibre_by_country('BG')
-    calibre_by_country('BH')
-    calibre_by_country('BM')
-    calibre_by_country('CZ')
-    calibre_by_country('EG')
-    calibre_by_country('FI')
-    calibre_by_country('GR')
-    calibre_by_country('KH')
-    calibre_by_country('ME')
-    calibre_by_country('PL')
-    calibre_by_country('PT')
-    calibre_by_country('RS')
-    calibre_by_country('RW')
-    calibre_by_country('UA')
+    
+    # Complete list of ISO 3166-1 alpha-2 country codes
+    country_codes = [
+        'AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AO', 'AR', 'AT', 'AU', 'AZ',
+        'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BN', 'BO', 'BR',
+        'BS', 'BT', 'BW', 'BY', 'BZ', 'CA', 'CD', 'CF', 'CG', 'CH', 'CI', 'CL',
+        'CM', 'CN', 'CO', 'CR', 'CU', 'CV', 'CY', 'CZ', 'DE', 'DJ', 'DK', 'DM',
+        'DO', 'DZ', 'EC', 'EE', 'EG', 'ER', 'ES', 'ET', 'FI', 'FJ', 'FM', 'FR',
+        'GA', 'GB', 'GD', 'GE', 'GH', 'GM', 'GN', 'GQ', 'GR', 'GT', 'GW', 'GY',
+        'HN', 'HR', 'HT', 'HU', 'ID', 'IE', 'IL', 'IN', 'IQ', 'IR', 'IS', 'IT',
+        'JM', 'JO', 'JP', 'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 'KP', 'KR', 'KW',
+        'KZ', 'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY',
+        'MA', 'MC', 'MD', 'ME', 'MG', 'MH', 'MK', 'ML', 'MM', 'MN', 'MR', 'MT',
+        'MU', 'MV', 'MW', 'MX', 'MY', 'MZ', 'NA', 'NE', 'NG', 'NI', 'NL', 'NO',
+        'NP', 'NR', 'NZ', 'OM', 'PA', 'PE', 'PG', 'PH', 'PK', 'PL', 'PT', 'PW',
+        'PY', 'QA', 'RO', 'RS', 'RU', 'RW', 'SA', 'SB', 'SC', 'SD', 'SE', 'SG',
+        'SI', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS', 'ST', 'SV', 'SY', 'SZ',
+        'TD', 'TG', 'TH', 'TJ', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TW',
+        'TZ', 'UA', 'UG', 'US', 'UY', 'UZ', 'VA', 'VC', 'VE', 'VN', 'VU', 'WS',
+        'YE', 'ZA', 'ZM', 'ZW'
+    ]
+    
+    print(f"Processing {len(country_codes)} countries...")
+    logging.info(f"Processing {len(country_codes)} countries")
+    
+    # Process each country code
+    for country_code in sorted(country_codes):
+        print(f"\nProcessing country: {country_code}")
+        logging.info(f"Processing country: {country_code}")
+        calibre_by_country(country_code)
     
 ##############################################
 # Call book_search Function for each Country #
 ##############################################
 if run_book_search:
-    print ("Running run_book_search...")
+    print("Running run_book_search...")
     logging.info("Running run_book_search...")
-    book_search('US')
-    book_search('CN')
-    book_search('DE')
-    book_search('GB')
-    book_search('ES')
-    book_search('GB')
-    book_search('SG')
-    book_search('JP')
-    book_search('ID')
-    book_search('CA')
-    book_search('HK')
-    book_search('AE')
-    book_search('ES')
-    book_search('IT')
-    book_search('KR')
-    book_search('OM')
-    book_search('FR')
-    book_search('NL')
-    book_search('TH')
-    book_search('PH')
-    book_search('MY')
-    book_search('AU')
-    book_search('IQ')
-    book_search('SA')
-    book_search('PK')
-    book_search('NP')
-    book_search('ZA')
-    book_search('IN')
-    book_search('RU')
-    book_search('NZ')
-    book_search('QA')
-    book_search('AR')
-    book_search('MX')
-    book_search('TW')
-    book_search('CH')
-    book_search('CL')
-    book_search('HU')
-    book_search('KE')
-    book_search('SE')
-    book_search('BR')
-    book_search('VN')
-    book_search('BE')
-    book_search('IE')
-    book_search('NO')
-    book_search('PE')
-    book_search('AT')
-    book_search('BA')
-    book_search('BG')
-    book_search('BH')
-    book_search('BM')
-    book_search('CZ')
-    book_search('EG')
-    book_search('FI')
-    book_search('GR')
-    book_search('KH')
-    book_search('ME')
-    book_search('PL')
-    book_search('PT')
-    book_search('RS')
-    book_search('RW')
-    book_search('UA')
     
+    # Complete list of ISO 3166-1 alpha-2 country codes
+    country_codes = [
+        'AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AO', 'AR', 'AT', 'AU', 'AZ',
+        'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BN', 'BO', 'BR',
+        'BS', 'BT', 'BW', 'BY', 'BZ', 'CA', 'CD', 'CF', 'CG', 'CH', 'CI', 'CL',
+        'CM', 'CN', 'CO', 'CR', 'CU', 'CV', 'CY', 'CZ', 'DE', 'DJ', 'DK', 'DM',
+        'DO', 'DZ', 'EC', 'EE', 'EG', 'ER', 'ES', 'ET', 'FI', 'FJ', 'FM', 'FR',
+        'GA', 'GB', 'GD', 'GE', 'GH', 'GM', 'GN', 'GQ', 'GR', 'GT', 'GW', 'GY',
+        'HN', 'HR', 'HT', 'HU', 'ID', 'IE', 'IL', 'IN', 'IQ', 'IR', 'IS', 'IT',
+        'JM', 'JO', 'JP', 'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 'KP', 'KR', 'KW',
+        'KZ', 'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY',
+        'MA', 'MC', 'MD', 'ME', 'MG', 'MH', 'MK', 'ML', 'MM', 'MN', 'MR', 'MT',
+        'MU', 'MV', 'MW', 'MX', 'MY', 'MZ', 'NA', 'NE', 'NG', 'NI', 'NL', 'NO',
+        'NP', 'NR', 'NZ', 'OM', 'PA', 'PE', 'PG', 'PH', 'PK', 'PL', 'PT', 'PW',
+        'PY', 'QA', 'RO', 'RS', 'RU', 'RW', 'SA', 'SB', 'SC', 'SD', 'SE', 'SG',
+        'SI', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS', 'ST', 'SV', 'SY', 'SZ',
+        'TD', 'TG', 'TH', 'TJ', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TW',
+        'TZ', 'UA', 'UG', 'US', 'UY', 'UZ', 'VA', 'VC', 'VE', 'VN', 'VU', 'WS',
+        'YE', 'ZA', 'ZM', 'ZW'
+    ]
+    
+    print(f"Processing {len(country_codes)} countries...")
+    logging.info(f"Processing {len(country_codes)} countries")
+    
+    # Process each country code
+    for country_code in sorted(country_codes):
+        print(f"\nProcessing country: {country_code}")
+        logging.info(f"Processing country: {country_code}")
+        book_search(country_code)
+
 ####################################
 # Call check_calibre_list Function #
 ####################################
